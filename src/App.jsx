19a1463e30b73f9fb501c0c3d0e11b6e7e5e63e4
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-11 01:11:45 +0530

    Delete pet component added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddPet from './components/AddPet'
 import ViewPet from './components/ViewPet'
 import SearchPet from './components/SearchPet'
+import DeletePet from './components/DeletePet'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
           <Route path='/' element={<AddPet />} />
           <Route path='/view' element={<ViewPet />} />
           <Route path='/search' element={<SearchPet />} />
+          <Route path='/delete' element={<DeletePet />} />
 
         </Routes>
       </BrowserRouter>

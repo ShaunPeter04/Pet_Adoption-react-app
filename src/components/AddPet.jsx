310645c601f5f8ddd1dce7e4f9af944fd6cbb70d
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-11 00:40:22 +0530

    Add pet component added

---
+++ src/components/AddPet.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+const AddPet = () => {
+  return (
+    <div>
+        
+        <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Pet ID</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Pet Name</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Species</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="genderSelect" className="form-label">Gender</label>
+                                <select id="genderSelect" className="form-select">
+                                    <option value="">Select Gender</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                    <option value="Other">Other</option>
+                                </select>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Class</label>
+                                <input type="number" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Section</label>
+                                <input type="text" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Guardian Name</label>
+                                <input type="number" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Contact Number</label>
+                                <input type="number" className="form-control" />
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Email Address</label>
+                                <input type="email" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <label htmlFor="" className="form-label">Address</label>
+                                <textarea name="" id="" className="form-control"></textarea>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <button className="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+    </div>
+  )
+}
+
+export default AddPet
